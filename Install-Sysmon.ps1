2021c8c389b3e951dae5207bc3f5ab03ec821928
Cyber74-Brian-McCaleb <102765888+Cyber74-Brian-McCaleb@users.noreply.github.com>
2023-01-04 11:34:23 -0800

    Update Install-Sysmon.ps1

--- Install-Sysmon.ps1
+++ Install-Sysmon.ps1
@@ -7,7 +7,7 @@ PowerShell script or module to install Sysmon with configuration
 .PARAMETER path
 The path to the working directory.  Default is user Documents.
 .EXAMPLE
-Install-Sysmon -path C:\Users\example\Desktop
+Install-Sysmon -path C:\Temp
 #>
 
 [CmdletBinding()]
